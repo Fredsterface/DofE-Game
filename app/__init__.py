c5e1809cb1085288306d1329959d36512f8691ad
Fredsterface <freddiestather@gmail.com>
2020-02-29 18:41:49 +0000

    Modified run.py and other files to make flask interface work

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
 from flask import Flask
 from classes import Room, Person, Item, GameState, directions
-from map import rooms
+from map import startRoom
 P = Person()
-G = GameState(P, rooms[1])
+G = GameState(P, startRoom)
 
 app = Flask(__name__)
 app.config['Game'] = G
