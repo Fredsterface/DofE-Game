275e9d4f0800611f79025df8b7e01ac3e5bd0b8d
Fredsterface <freddiestather@gmail.com>
2020-03-01 15:34:36 +0000

    Modified web interface

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,15 @@ sys.path.insert(0,parentdir)
 from flask import Flask
 from classes import Room, Person, Item, GameState, directions
 from map import startRoom
-P = Person()
-G = GameState(P, startRoom)
+from io import StringIO
 
 app = Flask(__name__)
+app.secret_key = 'Prince Philip is my hero'
+
+P = Person()
+G = GameState(P, startRoom)
 app.config['Game'] = G
 app.config['Directions'] = directions
+sys.stdout = app.config['stdout'] = StringIO()
+app.config['stdoutLength'] = len(app.config['stdout'].getvalue())
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
 from app import app
 from game import parseUserInput
+from classes import Room, Person, Item, GameState, directions
+from map import startRoom
+
 
 def formatText(text):
     text = text.split('\n')
@@ -10,14 +13,25 @@ def formatText(text):
     return str(ret)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    print(1)
     G = app.config['Game']
     return render_template('index.html', G=G)
 
 @app.route('/', methods=['POST'])
+@app.route('/index', methods=['POST'])
 def my_form_post():
     userInput = request.form['text'].lower().split()
     G = app.config['Game']
     parseUserInput(G, userInput)
+    if len(app.config['stdout'].getvalue()) != app.config['stdoutLength']:
+        flash(app.config['stdout'].getvalue()[app.config['stdoutLength']:])
+    app.config['stdoutLength'] = len(app.config['stdout'].getvalue())
     return render_template('index.html', G=G)
+
+@app.route('/newgame')
+def newGame():
+    P = Person()
+    G = GameState(P, startRoom)
+    app.config['Game'] = G
+    return redirect(url_for('index'))
